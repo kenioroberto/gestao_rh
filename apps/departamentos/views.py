74d99647a178462889b303f530ed761771a07f60
kenioroberto <kenioroberto@gmail.com>
2022-07-27 10:13:02 -0300

    Correção Departamento

--- apps/departamentos/views.py
+++ apps/departamentos/views.py
@@ -1,5 +1,3 @@
-from msilib.schema import ListView
-from django.shortcuts import render
 from django.urls import reverse_lazy
 from django.views.generic import ListView, CreateView, UpdateView, DeleteView
 from .models import Departamento
