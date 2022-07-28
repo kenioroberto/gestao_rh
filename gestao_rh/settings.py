e4381be542c2533c4b19569f928057bcc81caaa4
kenioroberto <kenioroberto@gmail.com>
2022-07-27 22:16:49 -0300

    Adicionando static comenta

--- gestao_rh/settings.py
+++ gestao_rh/settings.py
@@ -135,7 +135,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 STATIC_URL = '/static/'
 
-STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
+# STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
 
 STATIC_ROOT = os.path.join(BASE_DIR, "static/")
 
