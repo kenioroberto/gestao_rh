e06f18584171d0d618a53d6530d9e33589917ff6
kenioroberto <kenioroberto@gmail.com>
2022-07-27 21:34:10 -0300

    Adicionando ROOT

--- gestao_rh/settings.py
+++ gestao_rh/settings.py
@@ -135,12 +135,12 @@ MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 STATIC_URL = '/static/'
-# STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATICFILES_DIRS = [
     BASE_DIR / "static",
 ]
 
-STATIC_ROOT = os.path.join(BASE_DIR, "static/")
+
 
 LOGIN_REDIRECT_URL = 'home'
 
